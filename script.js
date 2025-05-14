20252bba0b74bb54f0c10e0db3d4799ac6d6e04b
Steve-cadet <fulstxckdevo@gmail.com>
2025-05-14 10:09:09 +0530

    updated working template

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("JavaScript is working!");
+document.addEventListener("DOMContentLoaded", () => {
+  const img = document.querySelector("img.lazyload");
+  const highResSrc = img.getAttribute("data-src");
+
+  const highResImage = new Image();
+  highResImage.src = highResSrc;
+
+  highResImage.onload = () => {
+    img.src = highResSrc;
+    img.style.filter = "none"; // remove blur once loaded
+  };
+});
